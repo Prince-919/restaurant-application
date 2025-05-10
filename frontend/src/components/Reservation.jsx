f3612cd8fbafd4ec169acb0e2260ca263a38b661
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-05-10 17:00:36 +0530

    change api url

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8000/api/v1/reservation/send",
+        "https://restaurant-api-purg.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
